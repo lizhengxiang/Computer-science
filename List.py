ef2dbc3afa277ea3e81c9bbba1bb84dff12dfa51
lizhengxiang <1065482100@qq.com>
2015-04-06 12:42:53 +0800

    python list

---
+++ List.py
@@ -0,0 +1,14 @@
+d = "lizhengxiang"
+c = "lizhengxiang"
+a = [1, d, 3, 4, 5, c, 7, 8, 9]
+print(a[0])
+print(a[5])
+print(a)
+a[0] = "lizhengxiang"
+b = [4, 5, 6]
+a[2] = [1,2,b] 
+m = a[2]
+m[0] = 7
+print(a[2])
+print(m)
+print(a)
